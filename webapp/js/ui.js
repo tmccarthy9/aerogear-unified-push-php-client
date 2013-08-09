ba2a2e1d373116933883ee60e22bd9acaa0e9ac3
Tommy McCarthy <tomccart@tomccart.csb>
2013-08-09 18:09:25 -0400

    Dynamically make input fields

--- webapp/js/ui.js
+++ webapp/js/ui.js
@@ -9,3 +9,37 @@ function showOptions()
 	var selOptions = document.getElementById('selectedSendOptions');
 	selOptions.style.display = 'block';
 }
+
+function addMessages()
+{
+	var container = document.getElementById('messageHolder');
+
+	var input = document.createElement("input");
+	input.type = "text";
+	input.name = "messageKey[]";
+	container.appendChild(input);
+		container.appendChild(document.createTextNode(" : "));
+	var input = document.createElement("input");
+	input.type = "text";
+	input.name = "messageValue[]";
+	container.appendChild(input);
+	// Append a line break 
+	container.appendChild(document.createElement("br"));
+}
+
+function addSimplePush()
+{
+	var container = document.getElementById('simplePushHolder');
+
+	var input = document.createElement("input");
+	input.type = "text";
+	input.name = "simplePushKey[]";
+	container.appendChild(input);
+		container.appendChild(document.createTextNode(" : "));
+	var input = document.createElement("input");
+	input.type = "text";
+	input.name = "simplePushValue[]";
+	container.appendChild(input);
+	// Append a line break 
+	container.appendChild(document.createElement("br"));
+}
